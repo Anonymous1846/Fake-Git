848561414ac6f6324afdc685be6ac356fcdb1488
Sharath Sunil <alphatrinity17@gmail.com>
2026-04-28 00:29:55 +0530

    Update: issue fixed

--- commands.cpp
+++ commands.cpp
@@ -1,15 +1,16 @@
 #include "commands.h"
 #include<filesystem>
 
+bool create_directory(std::string);
 
 int init(){
 	if(!create_directory(".git/objects/pack") && !create_directory(".git/objects/info") && !create_directory(".git/refs/heads") && !create_directory(".git/refs/tags")) return 1;
 	return 0;	
 }
 
-bool create_directory(string path){
+bool create_directory(std::string path){
 	std::error_code ec;
 	std::filesystem::create_directories(path, ec);
 	if(ec) return false;
-	return true
+	return true;
 }
