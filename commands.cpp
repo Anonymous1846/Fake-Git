28e3e23e81744bfe6978effe11d47ecf48e1aaa7
Sharath Sunil <alphatrinity17@gmail.com>
2026-04-28 07:40:56 +0530

    Update: written the directory creation logic and fiel write

--- commands.cpp
+++ commands.cpp
@@ -1,16 +1,30 @@
 #include "commands.h"
 #include<filesystem>
+#include<fstream>
 
-bool create_directory(std::string);
+
+bool create_init_directory(std::string);
 
 int init(){
-	if(!create_directory(".git/objects/pack") && !create_directory(".git/objects/info") && !create_directory(".git/refs/heads") && !create_directory(".git/refs/tags")) return 1;
-	return 0;	
+	if(!create_init_directory(".fake-git/objects/pack"))return 1;
+       	if(!create_init_directory(".fake-git/objects/info"))return 1;
+       	if(!create_init_directory(".fake-git/refs/heads"))return 1;
+       	if(!create_init_directory(".fake-git/refs/tags"))return 1;
+	std::ofstream file(".fake-git/HEAD");
+	if(!file){
+		return 1;
+	}
+	if()
+	return 0;
 }
 
-bool create_directory(std::string path){
+bool create_init_directory(std::string path){
 	std::error_code ec;
 	std::filesystem::create_directories(path, ec);
 	if(ec) return false;
 	return true;
+}
+
+bool write_contents(std::ofstream fileRef, std::string content){
+	
 }
