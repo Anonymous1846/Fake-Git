b7773a69a338326773f2a48380ae7259d6dbd3b0
Sharath Sunil <alphatrinity17@gmail.com>
2026-04-29 00:06:53 +0530

    Update: usage of string constants and var constants

--- commands.cpp
+++ commands.cpp
@@ -6,11 +6,11 @@
 bool create_init_directory(std::string);
 
 int init(){
-	if(!create_init_directory(".fake-git/objects/pack"))return 1;
-       	if(!create_init_directory(".fake-git/objects/info"))return 1;
-       	if(!create_init_directory(".fake-git/refs/heads"))return 1;
-       	if(!create_init_directory(".fake-git/refs/tags"))return 1;
-	std::ofstream file(".fake-git/HEAD");
+	if(!create_init_directory(Git::MAIN_DIR / "objects" / "pack"))return 1;
+       	if(!create_init_directory(Git::MAIN_DIR/ "objects" / "info"))return 1;
+       	if(!create_init_directory(Git::MAIN_DIR / "refs" / "heads"))return 1;
+       	if(!create_init_directory(Git::MAIN_DIR / "refs"/"tags"))return 1;
+	std::ofstream file(Git::MAIN_DIR / "HEAD");
 	if(!file){
 		return 1;
 	}
@@ -18,13 +18,13 @@ int init(){
 	return 0;
 }
 
-bool create_init_directory(std::string path){
+bool create_init_directory(std::filesystem &path){
 	std::error_code ec;
 	std::filesystem::create_directories(path, ec);
 	if(ec) return false;
 	return true;
 }
 
-bool write_contents(std::ofstream fileRef, std::string content){
-	
+bool write_contents(std::ofstream& fileRef, std::string& content){
+	fileRef << content;
 }
