50ac86479298c2256f90a5aae041e240d7c8980a
Sharath Sunil <alphatrinity17@gmail.com>
2026-05-01 13:26:40 +0530

    Update: bug fixes in the directory creation and content writing

--- commands.cpp
+++ commands.cpp
@@ -1,9 +1,12 @@
 #include "commands.h"
+#include "constants.h"
 #include<filesystem>
 #include<fstream>
 
 
-bool create_init_directory(std::string);
+bool create_init_directory(const std::filesystem::path&);
+bool write_contents(std::ofstream& ,const std::string& );
+
 
 int init(){
 	if(!create_init_directory(Git::MAIN_DIR / "objects" / "pack"))return 1;
@@ -14,17 +17,18 @@ int init(){
 	if(!file){
 		return 1;
 	}
-	if()
 	return 0;
 }
 
-bool create_init_directory(std::filesystem &path){
+bool create_init_directory(const std::filesystem::path& filePath){
 	std::error_code ec;
-	std::filesystem::create_directories(path, ec);
+	std::filesystem::create_directories(filePath, ec);
 	if(ec) return false;
 	return true;
 }
 
-bool write_contents(std::ofstream& fileRef, std::string& content){
+bool write_contents(std::ofstream& fileRef,const std::string& content){
 	fileRef << content;
+	if(!fileRef) return false;
+	return true;
 }
