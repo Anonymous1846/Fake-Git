7ce5b627f77747b80567d6e938f9ae348a395519
Sharath Sunil <alphatrinity17@gmail.com>
2026-05-01 21:52:57 +0530

    Update: added fix for file writing and config generation

--- commands.cpp
+++ commands.cpp
@@ -2,22 +2,26 @@
 #include "constants.h"
 #include<filesystem>
 #include<fstream>
+#include<iostream>
+
 
 
 bool create_init_directory(const std::filesystem::path&);
 bool write_contents(std::ofstream& ,const std::string& );
-
 
 int init(){
 	if(!create_init_directory(Git::MAIN_DIR / "objects" / "pack"))return 1;
        	if(!create_init_directory(Git::MAIN_DIR/ "objects" / "info"))return 1;
        	if(!create_init_directory(Git::MAIN_DIR / "refs" / "heads"))return 1;
        	if(!create_init_directory(Git::MAIN_DIR / "refs"/"tags"))return 1;
-	std::ofstream file(Git::MAIN_DIR / "HEAD");
-	if(!file){
-		return 1;
-	}
-	return 0;
+		std::ofstream headFile(Git::MAIN_DIR / "HEAD");
+		if(!headFile) return 1;
+		if(!write_contents(headFile,"ref: refs/heads/master\n"))return 1;
+		std::ofstream configFile(Git::MAIN_DIR / "config");
+		if(!configFile) return 1;
+		std::string configData = "[core]\n\trepositoryformatversion = 0\n\tfilemode - true\n\tbare = false\n";
+		if(!write_contents(configFile, configData)) return 1;
+		return 0;
 }
 
 bool create_init_directory(const std::filesystem::path& filePath){
@@ -28,6 +32,7 @@ bool create_init_directory(const std::filesystem::path& filePath){
 }
 
 bool write_contents(std::ofstream& fileRef,const std::string& content){
+	if(!fileRef) return false;
 	fileRef << content;
 	if(!fileRef) return false;
 	return true;
