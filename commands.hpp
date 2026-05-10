730dd23b526cc6c94efdbcbf8d45ebfad095a6dc
Sharath Sunil <alphatrinity17@gmail.com>
2026-05-10 16:08:11 +0530

    Update: added necessary declarations

--- commands.hpp
+++ commands.hpp
@@ -1,6 +1,8 @@
 #pragma once 
 
 #include<filesystem>
-
+#include<vector>
+#include <cstring>
 int init();
-std::string hash_object(std::filesystem::path& );
+std::string hash_object(const std::string& ,const std::filesystem::path& );
+int write_object(const std::string& ,const std::vector<uint8_t>& );
