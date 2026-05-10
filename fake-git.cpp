b09adef42381c499fedd87030877df0d68d0b9f9
Sharath Sunil <alphatrinity17@gmail.com>
2026-05-10 20:56:51 +0530

    Update: test workflow changed

--- fake-git.cpp
+++ fake-git.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<zlib.h>
 #include "commands.hpp"
 #include"sha.hpp"
+#include <cstring>
 
 int main(int argc,char **argv){
 	if(argc < 2){
@@ -11,6 +13,20 @@ int main(int argc,char **argv){
 	if(operation == "init"){
 		init();
 	}
-
-
+	else if(operation == "hash-object"){
+		
+		if(argc == 3){
+			std::cout<< hash_object("blob",argv[2]) << "\n";
+		}
+		else if(argc == 4 && std::string(argv[2])=="-w"){
+			return hash_object_write("blob",argv[3]);
+		}
+		else{
+			std::cout<<"Usage: fake-git hash-object [OPTION] <file-name>"<<std::endl;
+			return 1;
+		}
+		
+		
+	}
+	
 }
