1b93ae4f593f949951ed56a244d0c075cde03ec1
Sharath Sunil <alphatrinity17@gmail.com>
2026-05-14 21:11:21 +0530

    Update: header modifications for function types

--- commands.hpp
+++ commands.hpp
@@ -9,5 +9,5 @@ int write_object(const std::string& ,const std::vector<uint8_t>& );
 int hash_object_write(const std::string& ,const std::filesystem::path& );
 std::vector<uint8_t> read_bytes(const std::filesystem::path &);
 std::vector<uint8_t> wrap(const std::string &, const std::vector<uint8_t> &);
-std::vector<uint8_t> cat_file(std::string& );
+int cat_file(const std::string&,const std::string& );
 std::vector<uint8_t> decompress(const std::vector<uint8_t>& );
